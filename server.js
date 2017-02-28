d838761519c569c82264d006392497b210c0af8b
Samson Alajede <samson.alajede@konga.com>
2017-02-28 10:17:00 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.log('Express server is listening on port ', PORT);
+});
