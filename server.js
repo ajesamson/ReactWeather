1c308c07d07cc161d14b9bb0b8ec8e4629c7bae7
Samson Alajede <samson.alajede@konga.com>
2017-03-01 23:23:31 +0100

    Optimizing for Heroku

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+    if (req.secure === 'https') {
+      res.redirect('http://' + req.hostname + req.url)
+    } else {
+      next();
+    }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
