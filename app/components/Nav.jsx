34105253274a234f52e7eb4c9e8382b1ee2f295c
Samson Alajede <samson.alajede@konga.com>
2017-03-07 08:43:54 +0100

    Added Foundation and jQuery

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,11 +3,17 @@ var {Link} = require('react-router');
 
 var Nav = (props) => {
   return (
-    <div>
-      <h2>Nav Component</h2>
-      <Link to="/" >Get Weather</Link>
-      <Link to="/about">About</Link>
-      <Link to="/examples">Examples</Link>
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li><Link to="/" >Get Weather</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/examples">Examples</Link></li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        
+      </div>
     </div>
   );
 }
